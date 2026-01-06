526ca0ab7b38dad2bf83575e9641a632af11b459
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2026-01-06 11:20:25 +0530

    Update Signup.js

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -14,7 +14,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:3002/api/auth/signup", {
+    const res = await fetch("https://zerodha-clone-backend-mwx5.onrender.com/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
