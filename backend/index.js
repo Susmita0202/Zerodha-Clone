ca55ed3fa455dc4181dc4bf513b58917517494bb
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2026-01-06 11:46:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://zerodha-clone-frontend-ctuv.onrender.com", "http://localhost:3001"],
     credentials: true,
   })
 );
